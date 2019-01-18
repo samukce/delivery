ed7e54c642b17b7799b0eb7085041b502d825654
Samuel Pereira <samuel.p.araujo@gmail.com>
2019-01-17 22:44:41 -0300

    Add base tools to test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { mount } from 'enzyme';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+describe('App component', () => {
+  let spySaveOrder, component, sandbox;
+
+    beforeEach(() => {
+        sandbox = sinon.sandbox.create();
+
+        spySaveOrder = sandbox.stub();
+
+        const wrapper = mount(<App />);
+        component = wrapper.find(App);
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it('should save order', () => {
+        expect(spySaveOrder).to.have.been.calledOnce;
+    });
+})
