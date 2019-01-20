367bd17f24f8fa03c0d66aee24e8a46ada53d88b
Samuel Pereira <samuel.p.araujo@gmail.com>
2019-01-20 20:15:42 -0300

    Expand autocomplete when focus the field

--- src/repository/ProductRepository.js
+++ src/repository/ProductRepository.js
@@ -2,9 +2,9 @@ export default class ProductRepository {
     static all() {
         return [
             { id: 1, description: 'Water', value: 3.50 },
-            { id: 2, description: 'Product', value: 3.50 },
-            { id: 3, description: 'Neblina', value: 3.50 },
-            { id: 4, description: 'Product 3', value: 3.50 }
+            { id: 2, description: 'Product', value: 4.00 },
+            { id: 3, description: 'Neblina', value: 8.70 },
+            { id: 4, description: 'Product 3', value: 11.20 }
         ];
     }
 }
