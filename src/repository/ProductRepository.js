a07bf73650c22674f4a1a8ec7f2162f953727d3d
Samuel Pereira <samuel.p.araujo@gmail.com>
2019-01-20 01:24:30 -0300

    List products from repository

---
+++ src/repository/ProductRepository.js
@@ -0,0 +1,8 @@
+export default class ProductRepository {
+    static all() {
+        return [
+            { id: 1, description: 'Product 1', value: 3.50 },
+            { id: 2, description: 'Product 2', value: 3.50 }
+        ];
+    }
+}
