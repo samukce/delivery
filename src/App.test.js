5c5737a3795862860fb597008cfed28f6eba7ed8
Samuel Pereira <samuel.p.araujo@gmail.com>
2019-01-26 19:08:16 -0300

    Test refactor change value scope

--- src/App.test.js
+++ src/App.test.js
@@ -79,30 +79,32 @@ describe('App place order', () => {
     expect(wrapper.find('#total_amount').props().title).to.equal('$7.00');
   });
 
-  it('should calculate change to value', () => {
-    componentRender.onProductsChange([
-      { product_id: 1, description: 'Water', value: 3.5, quantity: 2 },
-    ]);
-    wrapper.find('#change_to').simulate('change', { target: { name: 'change_to', value: 20 } } );
+  describe('calculate change to value', () => {
+    it('should calculate', () => {
+      componentRender.onProductsChange([
+        { product_id: 1, description: 'Water', value: 3.5, quantity: 2 },
+      ]);
+      wrapper.find('#change_to').simulate('change', { target: { name: 'change_to', value: 20 } } );
 
-    expect(wrapper.state('change_difference')).to.equal(13.00);
-  });
+      expect(wrapper.state('change_difference')).to.equal(13.00);
+    });
 
-  it('should be empty if value less then total amount', () => {
-    componentRender.onProductsChange([
-      { product_id: 1, description: 'Water', value: 3.5, quantity: 2 },
-    ]);
-    wrapper.find('#change_to').simulate('change', { target: { name: 'change_to', value: 6.0 } } );
+    it('should be empty if less then total amount', () => {
+      componentRender.onProductsChange([
+        { product_id: 1, description: 'Water', value: 3.5, quantity: 2 },
+      ]);
+      wrapper.find('#change_to').simulate('change', { target: { name: 'change_to', value: 6.0 } } );
 
-    expect(wrapper.state('change_difference')).to.equal(null);
-  });
+      expect(wrapper.state('change_difference')).to.equal(null);
+    });
 
-  it('should be empty if value equal to the total amount', () => {
-    componentRender.onProductsChange([
-      { product_id: 1, description: 'Water', value: 3.5, quantity: 2 },
-    ]);
-    wrapper.find('#change_to').simulate('change', { target: { name: 'change_to', value: 7.0 } } );
+    it('should be empty if equal to the total amount', () => {
+      componentRender.onProductsChange([
+        { product_id: 1, description: 'Water', value: 3.5, quantity: 2 },
+      ]);
+      wrapper.find('#change_to').simulate('change', { target: { name: 'change_to', value: 7.0 } } );
 
-    expect(wrapper.state('change_difference')).to.equal(null);
+      expect(wrapper.state('change_difference')).to.equal(null);
+    });
   });
 })
