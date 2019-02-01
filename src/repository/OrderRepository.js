8f0ec371d9bab071a9c00d8ea641cd1cd64ef068
Samuel Pereira <samuel.p.araujo@gmail.com>
2019-01-31 22:34:10 -0300

    Lowdb find an order by address

--- src/repository/OrderRepository.js
+++ src/repository/OrderRepository.js
@@ -1,5 +1,7 @@
+import DbFactory from './DbFactory';
+
 class OrderRepository {
-    constructor(db) {
+    constructor(db = DbFactory.dbAdapter()) {
         this.db = db;
     }
 
@@ -7,17 +9,16 @@ class OrderRepository {
         if (!address) return {};
 
         const orders = this.db.get('orders')
-                         .filter(order => {
-                            const index = order.address.toUpperCase().indexOf(address.toUpperCase());
-                            return index !== -1;
-                         })
-                         .value()
-                         .reduce(function(map, order) {
-                            map[order.address] = order;
-                            return map;
-                          }, {});
+            .filter(order => {
+                const index = order.address.toUpperCase().indexOf(address.toUpperCase());
+                return index !== -1;
+            })
+            .value()
+            .reduce(function(map, order) {
+                map[order.address] = order;
+                return map;
+            }, {});
 
-        console.log(orders);
         return orders;
     }
 }
