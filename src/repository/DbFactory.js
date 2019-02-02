cf0ed8096b95b069b36be9c994a7330007403916
Samuel Pereira <samuel.p.araujo@gmail.com>
2019-02-02 00:04:18 -0300

    Save the local database in appData directory

--- src/repository/DbFactory.js
+++ src/repository/DbFactory.js
@@ -1,17 +1,19 @@
 const low = require('lowdb');
 const Memory = require('lowdb/adapters/Memory');
 
+let remote;
 let FileSync;
-if (window && window.require)
+if (window && window.require){
     FileSync = window.require('lowdb/adapters/FileSync');
-
+    remote = window.require('electron').remote;
+}
 
 export default class DbFactory {
     static dbAdapter() {
         const db = low(
             process.env.NODE_ENV === 'test'
               ? new Memory()
-              : new FileSync('db.json')
+              : new FileSync(remote.getGlobal('settings').database_path)
         )
 
         db.defaults({ orders: [] })
