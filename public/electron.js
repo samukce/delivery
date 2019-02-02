7e47d29955c5bebb522fe506deecb38914799181
Samuel Pereira <samuel.p.araujo@gmail.com>
2019-02-02 13:56:29 -0300

    Repalce dublo quote to simple one as default

--- public/electron.js
+++ public/electron.js
@@ -1,9 +1,9 @@
-const electron = require("electron");
+const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
-const path = require("path");
-const isDev = require("electron-is-dev");
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 let mainWindow;
 
@@ -20,21 +20,21 @@ function createWindow() {
   });
   mainWindow.loadURL(
     isDev
-      ? "http://localhost:3000"
-      : `file://${path.join(__dirname, "../build/index.html")}`
+      ? 'http://localhost:3000'
+      : `file://${path.join(__dirname, '../build/index.html')}`
   );
-  mainWindow.on("closed", () => (mainWindow = null));
+  mainWindow.on('closed', () => (mainWindow = null));
 }
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
-app.on("activate", () => {
+app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
   }
--- src/setupTests.js
+++ src/setupTests.js
@@ -2,8 +2,8 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 configure({ adapter: new Adapter() });
 
-const chai = require("chai");
-const sinonChai = require("sinon-chai");
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
 require('chai/register-assert');  // Using Assert style
