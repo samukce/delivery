1cf4a22caec330979ff8d1cde3413caec07de424
samukce <samuel.p.araujo@gmail.com>
2020-11-26 15:05:07 -0300

    Notify client to update the app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { Route, Switch } from "react-router-dom";
 import CheckoutHome from "./CheckoutHome";
 import { I18nProvider } from "@lingui/react";
@@ -7,10 +7,59 @@ import { NotificationContainer } from "react-notifications";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Products from "./components/products/ProductList";
 import EditOrAddProduct from "./components/products/EditOrAddProduct";
+import { useSnackbar } from "notistack";
+import * as serviceWorker from "./serviceWorker";
+import { Button } from "@material-ui/core";
 
 function App(props) {
   const { language } = props;
   const localeMessage = require(`./locales/${language}/messages.js`);
+  const { enqueueSnackbar } = useSnackbar();
+  const [newVersionAvailable, setNewVersionAvailable] = useState(false);
+  const [waitingWorker, setWaitingWorker] = useState({});
+
+  useEffect(() => {
+    const onServiceWorkerUpdate = (registration) => {
+      setNewVersionAvailable(true);
+      setWaitingWorker(registration && registration.waiting);
+    };
+
+    const updateServiceWorker = () => {
+      waitingWorker && waitingWorker.postMessage({ type: "SKIP_WAITING" });
+      setNewVersionAvailable(false);
+      window.location.reload();
+    };
+
+    const refreshAction = (key) => {
+      //render the snackbar button
+      return (
+        <Fragment>
+          <Button
+            className="snackbar-button"
+            size="small"
+            color="primary"
+            variant="contained"
+            onClick={updateServiceWorker}
+          >
+            {"Atualizar"}
+          </Button>
+        </Fragment>
+      );
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      serviceWorker.register({ onUpdate: onServiceWorkerUpdate });
+    }
+
+    if (newVersionAvailable) {
+      //show snackbar with refresh button
+      enqueueSnackbar("Uma nova versão foi disponibilizada", {
+        persist: true,
+        variant: "success",
+        action: refreshAction(),
+      });
+    }
+  }, [newVersionAvailable, enqueueSnackbar, waitingWorker]);
 
   return (
     <I18nProvider language={language} catalogs={{ [language]: localeMessage }}>
--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
-import * as serviceWorker from "./serviceWorker";
 import "react-notifications/lib/notifications.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App";
+import { SnackbarProvider } from "notistack";
 
 ReactDOM.render(
   <Router>
     <Provider store={store}>
-      <App language="pt-BR" />
+      <SnackbarProvider>
+        <App language="pt-BR" />
+      </SnackbarProvider>
     </Provider>
   </Router>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.register();
